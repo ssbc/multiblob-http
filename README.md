491251b9184e071aa166e821fcccf895f38cf65d
Dominic Tarr <dominic.tarr@gmail.com>
2016-07-27 10:21:51 +1200

    work on improving caching

--- README.md
+++ README.md
@@ -1,6 +1,62 @@
 # multiblob-http
 
+serve content-addressed blobs over http.
+see use with [multiblob](https://github.com/ssbc/multiblob)
+
+# example
+
+``` js
+var MultiBlob = require('multiblob')
+var MultiBlobHttp = require('multiblob-http')
+var http = require('http')
+
+var dir = where_files_go //set this.
+
+var blobs = MultiBlob(dir)
+
+http.createServer(MultiBlobHttp(blobs, '/blobs')).listen(8000)
+```
+this will return an http handler (compatible with express middleware)
+that will handle requests
+ * `GET /blobs/get/{id}`
+ * `POST /blobs/add` (which will respond with the hash)
+
+you can also set a different prefix, but I use `/blobs`
+
+## http api
+
+### GET /get/{id}
+
+retrive blob with hash {id}
+
+### POST /add
+
+posts to add do not require to have a hash, but will respond with the hash.
+(TODO: take a POST to /add/{id} and error if received content did not have that hash)
+
+## caching && headers
+
+`multiblobs-http` provides the correct headers to make serving content-addressed
+files as efficient as possible.
+
+First the the `etag` header is set to the hash and the expires header is set to a year in the future.
+Ideally, the browser shouldn't request this resource again for a whole year.
+Probably it might revalidate it when someone uses `ctrl-R` to reload the page.
+When it does, it will request with `if-none-modified` set to the hash.
+Since content-addressed files are _never_ modified, the server immediately responds
+with 304 (not modified)
+
+the `content-length` header is always used. This way, if a connection fails somehow,
+or there is an error later, the browser should detect it.
+
+Of course, it would be way better if browsers just understood content-hashes.
+But, we have to play the hand we where delt, and
+`multiblob-http` makes the most of the broken web we live in.
+
 
 ## License
 
 MIT
+
+
+
--- index.js
+++ index.js
@@ -4,6 +4,18 @@ var toPull = require('stream-to-pull-stream')
 var qs = require('querystring')
 var URL = require('url')
 
+var YEAR = 60*60*24*365
+
+function headers(res, hash) {
+  //don't set cache control because (i think) that tells
+  //the browser to not keep the cache
+  res.setHeader('cache-control', 'maxage='+YEAR)
+  //expires makes the brower not even revalidate.
+  res.setHeader('expires', new Date(Date.now()+YEAR*1000).toISOString())
+  res.setHeader('expiry', new Date(Date.now()+YEAR*1000).toISOString())
+  res.setHeader('etag', hash)
+}
+
 //host blobs
 module.exports = function (blobs, url, opts) {
   opts = opts || {}
@@ -14,7 +26,7 @@ module.exports = function (blobs, url, opts) {
       res.end(JSON.stringify({error: true, status: 404}))
     }
 
-    if(req.url === url+'/add')
+    if(req.method === 'POST' && req.url === url+'/add' && opts.readonly !== true)
       pull(
         toPull(req),
         blobs.add(function (err, hash) {
@@ -22,35 +34,54 @@ module.exports = function (blobs, url, opts) {
         })
       )
     else if(req.url.indexOf(url+'/get/') == 0) {
+      if(!(req.method === "GET" || req.method == 'HEAD')) return next()
+
       var u = URL.parse('http://makeurlparseright.com'+req.url)
       var hash = decodeURIComponent(u.pathname.substring((url+'/get/').length))
       var q = qs.parse(u.query)
 
+
+      //if a browser revalidates, just tell them it hasn't changed, the hash has not changed.
+      if(req.headers['if-none-match'] === hash) {
+        headers(res, hash)
+        return res.writeHead(304), res.end()
+      }
+
       //enable cors by default
-      if(opts.cors !== false)
+      if(opts.cors !== false) {
         res.setHeader('Access-Control-Allow-Origin', '*')
+      }
 
-      if(q.filename)
-        res.setHeader('Content-Discosition', 'inline; filename='+q.filename)
-
-      if(q.gzip)
-        res.setHeader('Content-Encoding', 'gzip')
-
-      if(q.contentType)
-        res.setHeader('Content-Type', q.contentType)
-
-      blobs.has(hash, function (err, has) {
+      blobs.size(hash, function (err, size) {
         if(err) return next(err)
-        if(!has) return next(new Error('no blob:'+hash))
+        if(!size) return next(new Error('no blob:'+hash))
 
+        headers(res, hash)
+        res.setHeader('content-length', size)
+
+        if(q.filename)
+          res.setHeader('Content-Discosition', 'inline; filename='+q.filename)
+
+        if(q.gzip)
+          res.setHeader('Content-Encoding', 'gzip')
+
+        if(q.contentType)
+          res.setHeader('Content-Type', q.contentType)
+
+        //writing the status code now.
         res.writeHead(200)
+        if(req.method === 'HEAD') return res.end()
+
         pull(
           blobs.get(hash),
           //since this is an http stream, handle error the http way.
-          pull.through(null, function (err) {
-            if(err) throw err //DEBUG
-            if(err) next(err)
-          }),
+          //there is nothing we can do about an error now, since
+          //we already wrote the headers. but since we included content-length
+          //the client will know it went wrong.
+//          pull.through(null, function (err) {
+//            if(err) throw err //DEBUG
+//            if(err) next(err)
+//          }),
           toPull(res)
         )
       })
@@ -58,4 +89,28 @@ module.exports = function (blobs, url, opts) {
     else next()
   }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
